da5b46365b9ea36fb0f3a8d19449e08c2900fac8
andyh <andy.980f@gmail.com>
2021-12-19 16:17:23 -0600

    made a full type for the spad bit arrays

---
+++ vl53l0x_spadarray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andyh on 12/19/21.
+//
+
+#include "vl53l0x_spadarray.h"
---
+++ vl53l0x_spadarray.h
@@ -0,0 +1,8 @@
+//
+// Created by andyh on 12/19/21.
+//
+
+#ifndef VL53_VL53L0X_SPADARRAY_H
+#define VL53_VL53L0X_SPADARRAY_H
+
+#endif //VL53_VL53L0X_SPADARRAY_H
