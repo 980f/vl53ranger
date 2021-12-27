b2e1ca6dcdad85aeb665e72a4f36a956a64793ca
andyh <andy.980f@gmail.com>
2021-12-27 12:38:51 -0600

    two of six processes coded

--- nonBlocking.cpp
+++ nonBlocking.cpp
@@ -3,3 +3,257 @@
 //
 
 #include "nonBlocking.h"
+
+//from safely/cppext lib:
+template<typename Scalar> Scalar take(Scalar &owner) {
+  Scalar taken = owner;
+  owner = static_cast<Scalar>(0);
+  return taken;
+}
+
+using namespace VL53L0X; //# this file exists to manage entities from this namespace
+
+void NonBlocking::inLoop() {
+  if (waitOnStart) {
+    Erroneous<uint8_t> flags;
+    fetch(flags, REG_SYSRANGE_START);
+    if (flags.isOk()) {
+      if (getBit<0>(flags.wrapped) == 0) {
+        waitOnStart = 0;
+      } else {
+        if (--waitOnStart == 0) {
+          //toodo: abandon pending process and measurements
+        }
+      }
+    } else {
+      //todo: handle error on waiting for start, most likely same as timeout.
+    }
+  }
+  if (waitOnMeasurementComplete) {
+    Erroneous<bool> value;
+    value = GetMeasurementDataReady();
+    if (value.isOk()) {
+      if (value == 1) {
+        waitOnMeasurementComplete = 0;
+        doMeasurementComplete(true);
+      } else {
+        if (--waitOnMeasurementComplete == 0) {
+          //todo: log timeout error
+          doMeasurementComplete(false);
+        }
+      }
+    } else {
+      //todo: decide if we stop on communications error, not sure if st code did/
+    }
+  }
+  //No else here so that the meas complete action can invoke a waitOnStop and get a quick check
+  if (waitOnStop) {
+    auto StopCompleted = GetStopCompletedStatus();
+    if (StopCompleted.isOk()) {
+      if (StopCompleted == 0x00) {
+        auto climask = ClearInterruptMask(GPIOFUNCTIONALITY_NEW_MEASURE_READY);//copied from adafruit copy of st advice
+        onStopComplete(true);
+      } else {
+        if (--waitOnStop == 0) {
+          onStopComplete(false);
+        }
+      }
+    } else {
+      //todo: handle comm error.
+    }
+  }
+  //todo: if action request act on it:
+}
+
+void NonBlocking::onStopComplete(bool b) {
+  //todo: notify stopped
+}
+
+void NonBlocking::doMeasurementComplete(bool successful) {
+  if (measurementInProgress == Abandoned) {
+    return;
+  }
+  if (!successful) {
+    //todo: handle failed measurement, most likely abandon all processes and mark 'need init'
+  }
+  auto measok = GetRangingMeasurementData(theRangingMeasurementData);
+  if (measok == VL53L0X::ERROR_NONE) {
+    theLastMeasurement = take(measurementInProgress);
+    switch (theLastMeasurement) {
+      case forXtalk:
+        if (theXtalkProcess.onMeasurement(theRangingMeasurementData)) {
+          requestMeasurement(forXtalk);
+        } else {
+          //todo: do we notify on xtalk meas complete?
+        }
+        return;
+      case Abandoned:
+        return; //already handled prior to switch
+      case forRange:
+        break;
+      case forVHV:
+        break;
+      case forPhase:
+        break;
+      case forRef:
+        break;
+      case forOffset:
+        break;
+    }
+  } else {
+    measurementInProgress = Abandoned;
+    //todo: notify process failure
+  }
+}
+
+bool NonBlocking::startMeasurement(NonBlocking::MeasurementAction action) {
+  //any setting of seqconfig or the like has been done
+  //if not continuous then:
+
+  measurementInProgress = action;
+  return true;
+}
+
+bool NonBlocking::XtalkProcess::begin() {
+  if (XTalkCalDistance.raw <= 0) {//ICK: type was unsigned, and so this is a compare to zero.
+//        return ERROR_INVALID_PARAMS;
+    return false;
+  }
+/* Disable the XTalk compensation */
+  dev.SetXTalkCompensationEnable(0);
+  /* Disable the RIT */
+  dev.SetLimitCheckEnable(CHECKENABLE_RANGE_IGNORE_THRESHOLD, 0);
+  measurementRemaining = 50;
+  dev.startMeasurement(forXtalk);
+}
+
+bool NonBlocking::XtalkProcess::onMeasurement(const RangingMeasurementData_t &RangingMeasurementData) {
+  //process each:
+  if (RangingMeasurementData.rangeError == 0) {
+    sum_ranging += RangingMeasurementData.RangeMilliMeter;
+    sum_signalRate += RangingMeasurementData.SignalRateRtnMegaCps;
+    sum_spads += RangingMeasurementData.EffectiveSpadRtnCount.rounded();//980f: formerly truncated
+    ++total_count;
+  } else {
+    //todo: abend the process
+    return false;
+  }
+
+  if (--measurementRemaining == 0) {   //post process
+    /* no valid values found */
+    if (total_count == 0) {
+//todo          return ERROR_RANGE_ERROR;
+    }
+    /* FixPoint1616_t / uint16_t = FixPoint1616_t */
+    FixPoint1616_t xTalkStoredMeanSignalRate(sum_signalRate, total_count, 0);//ick: was not rounded prior to 980f
+    FixPoint1616_t xTalkStoredMeanRange(sum_ranging, total_count);//round the division
+    FixPoint1616_t xTalkStoredMeanRtnSpads(sum_spads, total_count);
+
+    /* Round Mean Spads to Whole Number.
+     * Typically the calculated mean SPAD count is a whole number or very close to a whole number,
+     * therefore any truncation will not result in a significant loss in accuracy.
+     * Also, for a grey target at a typical distance of around 400mm, around 220 SPADs will be enabled,
+     * therefore any truncation will result in a loss of accuracy of less than 0.5%.
+     */
+    uint32_t xTalkStoredMeanRtnSpadsAsInt = roundedScale(xTalkStoredMeanRtnSpads, 16);
+
+    /* Round Cal Distance to Whole Number.
+     * Note that the cal distance is in mm, therefore no resolution is lost.*/
+    uint32_t xTalkCalDistanceAsInt = roundedScale(XTalkCalDistance, 16);
+    FixPoint1616_t XTalkCompensationRateMegaCps;
+
+    if (xTalkStoredMeanRtnSpadsAsInt == 0 || xTalkCalDistanceAsInt == 0 || xTalkStoredMeanRange >= XTalkCalDistance) {
+      XTalkCompensationRateMegaCps = 0.0F;
+    } else {
+      /* Apply division by mean spad count early in the calculation to keep the numbers small.
+       * This ensures we can maintain a 32bit calculation.
+       * Fixed1616 / int := Fixed1616 */
+      uint32_t signalXTalkTotalPerSpad = (xTalkStoredMeanSignalRate) / xTalkStoredMeanRtnSpadsAsInt;//ick: round divide
+
+      /* Complete the calculation for total Signal XTalk per SPAD
+       * Fixed1616 * (Fixed1616 - Fixed1616/int) := (2^16 * Fixed1616)     */
+      signalXTalkTotalPerSpad *= (Unity.raw - (xTalkStoredMeanRange / xTalkCalDistanceAsInt));//ick: round divide
+
+      /* Round from 2^16 * Fixed1616, to Fixed1616. */
+      XTalkCompensationRateMegaCps = roundedScale(signalXTalkTotalPerSpad, 16);
+    }
+
+    /* Enable the XTalk compensation */
+    dev.SetXTalkCompensationEnable(true);
+
+    /* Enable the XTalk compensation */
+    dev.SetXTalkCompensationRateMegaCps(XTalkCompensationRateMegaCps);
+
+    dev.logError(VL53L0X::ERROR_NONE, "Xtalk Process");
+    //todo: how shall we signal 'process complete'?
+    return false;
+  } else {
+    //caller makes the next request
+    return true;//
+  }
+}
+
+NonBlocking::XtalkProcess::XtalkProcess(NonBlocking &dev) : dev(dev) {
+}
+
+NonBlocking::OffsetProcess::OffsetProcess(NonBlocking &dev) : dev(dev) {
+}
+
+bool NonBlocking::OffsetProcess::begin() {
+  /* Get the value of the TCC */
+  SequenceStepWasEnabled = dev.GetSequenceStepEnable(SEQUENCESTEP_TCC);
+  /* Disable the TCC */
+   dev.SetSequenceStepEnable(SEQUENCESTEP_TCC, false);
+  /* Disable the RIT */
+  dev.SetLimitCheckEnable(CHECKENABLE_RANGE_IGNORE_THRESHOLD, false);
+  /* Perform 50 measurements and compute the averages */
+  sum_ranging = 0;
+  total_count = 0;
+  measurementRemaining=50;
+  //todo: actually fire off first measurement
+  return false;
+}
+
+bool NonBlocking::OffsetProcess::onMeasurement(const RangingMeasurementData_t &RangingMeasurementData) {
+  if (RangingMeasurementData.rangeError == VL53L0X::Range_Valid) {
+    sum_ranging += RangingMeasurementData.RangeMilliMeter;
+    ++total_count;
+  } else {
+    //todo: how to handle a range Error? until we try again:
+    dev.logError(VL53L0X::ERROR_RANGE_ERROR, __FUNCTION__);
+    return false;
+  }
+
+  /* no valid values found */
+  if (total_count == 0) {
+    dev.logError(VL53L0X::ERROR_RANGE_ERROR, __FUNCTION__ );
+    return false;
+  }
+  if (--measurementRemaining > 0) {
+    return true;
+  }
+
+  /* FixPoint1616_t / uint16_t = FixPoint1616_t */
+  FixPoint1616_t StoredMeanRange(sum_ranging, total_count);
+  int32_t StoredMeanRangeAsInt = StoredMeanRange.rounded();
+
+  /* Round Cal Distance to Whole Number.
+   * Note that the cal distance is in mm, therefore no resolution is lost.
+   * 980f: but why not retain the fractional part for tracability of the value? Rounding to resolution is unnecessary */
+  int32_t CalDistanceAsInt_mm = CalDistanceMilliMeter.rounded();
+  int32_t OffsetMicroMeter = (CalDistanceAsInt_mm - StoredMeanRangeAsInt) * 1000;
+
+  /* Apply the calculated offset */
+  dev.Data.CurrentParameters.RangeOffsetMicroMeters = OffsetMicroMeter;//record
+  auto ok= dev.SetOffsetCalibrationDataMicroMeter(OffsetMicroMeter);//send to device
+  if(!ok) {
+    dev.logError(ok ,"Offset Measurement failed on send to device");
+  } else {
+    dev.logError(VL53L0X::ERROR_NONE,__FUNCTION__ );
+  }
+  /* Restore the TCC */
+  if (SequenceStepWasEnabled) {
+    return dev.SetSequenceStepEnable(SEQUENCESTEP_TCC, true);
+  }
+  return false;//done
+}
--- nonBlocking.h
+++ nonBlocking.h
@@ -5,7 +5,124 @@
 #ifndef VL53_NONBLOCKING_H
 #define VL53_NONBLOCKING_H
 
-class nonBlocking {
+#include "vl53l0x_api.h"
+
+class NonBlocking: public VL53L0X::Api  {
+
+public:
+  VL53L0X::Error lastError=VL53L0X::ERROR_NONE;
+  const char *errorLocation= nullptr;
+
+  bool logError(VL53L0X::Error code,const char *location){
+    lastError=code;
+    errorLocation=location;
+    return false;
+  }
+
+#define ON_ERROR(c)   return logError( c , __FUNCTION__ );
+
+  NonBlocking(uint8_t i2c_addr = VL53L0X_I2C_ADDR>>1, TwoWire &i2c = Wire) : Api({i2c,i2c_addr,400})
+  ,theXtalkProcess(*this)
+  , theOffsetProcess(*this){
+    //do no real actions so that we can statically construct
+  }
+
+  enum MeasurementAction {
+    Abandoned = 0   //for state machine reset.
+    , forRange  //normal use, when not in continuous mode
+    , forVHV     //special seqconfig
+    , forPhase   //special seqconfig
+    , forRef     //special seqconfig
+    , forOffset  //50 times
+    , forXtalk   //50 times
+  };
+
+  virtual void onStopComplete(bool b);
+  //placeholder for what to do when a wait on measurement complete is successful
+  virtual void doMeasurementComplete(bool successful);
+
+  void waitForMeasurement(unsigned loops = 250) {
+    waitOnMeasurementComplete = loops;
+    //if functional rather than virtual here is where we record the action to take on completion.
+  }
+
+  void waitForStop(unsigned loops = 250) {
+    waitOnStop = loops;
+    //if functional rather than virtual here is where we record the action to take on completion.
+  }
+
+/** call this from your dispatcher, such as loop() in Arduino  or   while(1){WFE(); ... }*/
+  void inLoop();
+
+  bool requestMeasurement(MeasurementAction use){
+    //todo: check if any in progress and refuse, until we implement a queue
+    switch (use) {
+      case Abandoned:
+        return false;
+      case forXtalk:
+        return theXtalkProcess.begin();
+      case forRange:
+        break;
+      case forVHV:
+        break;
+      case forPhase:
+        break;
+      case forRef:
+        break;
+      case forOffset:
+        break;
+    }
+  }
+
+protected:
+  /** makes a buch of measurements then sets XTalkCompensationRateMegaCps.
+   * usage:    .theXtalkProcess.XTalkCalDistance= your value for it;  .requestMeasurement(forXtalk) */
+  class XtalkProcess {
+    NonBlocking &dev;
+  public:
+    FixPoint1616_t XTalkCalDistance;
+  private:
+    /* Perform 50 measurements and compute the averages */
+    unsigned sum_ranging = 0;//bug: former use of 16 bit begged for integer overflow
+    unsigned sum_spads = 0;//... which we will tolerate only on processors whose natural int is 16 bits
+    FixPoint1616_t sum_signalRate = 0;
+    uint32_t total_count = 0;//ick: unsigned is probably adequate
+    unsigned measurementRemaining = 0;
+  public:
+    XtalkProcess(NonBlocking &dev);
+    bool begin();
+    /** @returns whether to continue the process. if not then dev.lastError details why  */
+    bool onMeasurement(const VL53L0X::RangingMeasurementData_t &RangingMeasurementData);
+  } theXtalkProcess;
+
+  class OffsetProcess {
+  public:
+    OffsetProcess(NonBlocking &dev);
+  private:
+    NonBlocking &dev;
+  public:
+    FixPoint1616_t CalDistanceMilliMeter;
+  private:
+    bool SequenceStepWasEnabled=false;
+    uint16_t sum_ranging = 0;
+    uint32_t total_count = 0;
+    unsigned measurementRemaining=0;
+  public:
+    bool begin();
+    /** @returns whether to continue the process. if not then dev.lastError details why  */
+    bool onMeasurement(const VL53L0X::RangingMeasurementData_t &RangingMeasurementData);
+  } theOffsetProcess;
+
+  /** result of most recent measurement of any type */
+  VL53L0X::RangingMeasurementData_t theRangingMeasurementData;
+  MeasurementAction theLastMeasurement;
+
+public: //for debug, read only outside of this class.
+  MeasurementAction measurementInProgress=Abandoned;
+  unsigned waitOnStart=0;
+  unsigned waitOnMeasurementComplete = 0;
+  unsigned waitOnStop = 0;
+  bool startMeasurement(MeasurementAction action);
 };
 
 #endif //VL53_NONBLOCKING_H
